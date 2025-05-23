e814dade05f90eeaa6b64e6bb44816bd94992a49
Multarix <darien2105@gmail.com>
2025-05-23 18:53:13 +1000

    port no longer required, handled by CF

--- client/script.ts
+++ client/script.ts
@@ -34,7 +34,7 @@ function init(){
 		});
 	}
 
-	const socket = new WebSocket("ws:bingo.multarix.com:5683");
+	const socket = new WebSocket("ws:bingo.multarix.com");
 	socket.addEventListener("open", () => {
 		console.log("[Join] Connected to websocket");
 
