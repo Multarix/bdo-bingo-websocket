809ee4cd0af3f96070bdc37692a24d26a65fceeb
Multarix <darien2105@gmail.com>
2025-05-25 13:26:50 +1000

    semi colon

--- admin/script.ts
+++ admin/script.ts
@@ -3,8 +3,7 @@ interface UpdateMessage {
 	bingoBoard: string[]
 	hasHappened: string[]
 }
-
-type BingoToggleOptions = string[]
+type BingoToggleOptions = string[];
 
 
 /* ****************** */
@@ -22,8 +21,8 @@ function toggleItem(socket: WebSocket, content: string){
 	const toggleObject = {
 		auth: authToken,
 		toggle: content
-	}
-	
+	};
+
 	socket.send(JSON.stringify(toggleObject));
 }
 
@@ -35,8 +34,8 @@ function adminConnectToWebsocket(){
 
 		const connectObj = {
 			auth: authToken
-		}
-		
+		};
+
 		console.log("Sending: ", connectObj);
 		socket.send(JSON.stringify(connectObj)); // This will send back a message with all available toggles
 	});
@@ -46,28 +45,28 @@ function adminConnectToWebsocket(){
 		const message = event.data;
 		const obj: UpdateMessage | BingoToggleOptions = JSON.parse(message);
 		console.log("Recieved: ", obj);
-		
+
 		if(Array.isArray(obj)){ // It's a list of valid toggles
 			if(!isFirstConnect) return;
-			
+
 			for(const item of obj){
 				const button = document.createElement("button");
 				button.textContent = item;
 				button.onclick = toggleItem.bind(null, socket, item);
-				
+
 				buttonContainer.appendChild(button);
 			}
-			
+
 			isFirstConnect = false;
 			const initialLoad = document.getElementById("initialLoad") as HTMLDivElement;
 			initialLoad.style.display = "none";
 			return socket.send("{}");
 		}
 
-		
+
 		if(obj.bingoBoard){
 			if(buttonContainer.childElementCount === 0) return; // Initial message, page isn't setup yet.
-			
+
 			for(const child of buttonContainer.children){
 				const textContent = child.textContent as string;
 				child.className = (obj.hasHappened.includes(textContent)) ? "happened" : "";
