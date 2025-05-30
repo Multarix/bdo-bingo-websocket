77146afd9a1eb3fa14a36e6e139fcf637af4d717
Multarix <darien2105@gmail.com>
2025-05-30 17:04:40 +1000

    remove the dns update

--- server/main.ts
+++ server/main.ts
@@ -6,14 +6,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import * as f from "./functions.js";
-import updateDNSRecord from "./update_dns.js";
 
-const CHECK_INTERVAL = 10 * 60 * 1000; // 10 minutes
-
-// Initial run and schedule every 10 minutes
+// Initial run
 f.log("info", "Starting websocket server.");
-updateDNSRecord();
-const ipCheckInterval = setInterval(updateDNSRecord, CHECK_INTERVAL);
 
 const HOST_PORT = 8080;
 const wss = new WebSocketServer({ port: HOST_PORT });
@@ -31,5 +26,4 @@ wss.on("error", (error) => f.log("error", error.message));
 wss.on("listening", () => f.log("info", `Listening on port: ${chalk.greenBright(HOST_PORT)}`));
 wss.on("close", () => {
 	clearInterval(pingInterval);
-	clearInterval(ipCheckInterval);
 });
