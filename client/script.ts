17d757e0c3487a7ae8af6b3768643904e086fce3
Multarix <darien2105@gmail.com>
2025-06-18 18:51:41 +1000

    countdown

--- client/script.ts
+++ client/script.ts
@@ -1,3 +1,76 @@
+const startTime = 1751092200000;
+const countdown = document.getElementById("countdown") as HTMLElement;
+
+/**
+ * @name humanTime
+ * @param {Number} ms The number of milliseconds to convert
+ * @param {String} [format] The format to convert the time to, defaults to "\\h hours \\m minutes \\s seconds"
+ * @returns {String} The converted time
+ * @description Convert milliseconds to human readable formats
+ * - `\\S` = Miliseconds
+ * - `\\s` = Seconds
+ * - `\\m` = Minutes
+ * - `\\h` or `\\H` = Hours
+ * - `\\d` or `\\D` = Days
+ * - `\\M` = Months
+ * - `\\y` or `\\Y` = Years
+ * @example const format = humanTime(951000, "\\m minutes \\s seconds");
+ * console.log(format); // 15 minutes 51 seconds
+**/
+function humanTime(ms: number, format = "\\h hours \\m minutes \\s seconds"){
+	if(!format) return `${ms}ms`;
+
+	const baseMilliseconds = ms;
+	const baseSeconds = Math.floor(baseMilliseconds / 1000);
+	const baseMinutes = Math.floor(baseSeconds / 60);
+	const baseHours = Math.floor(baseMinutes / 60);
+	const baseDays = Math.floor(baseHours / 24);
+	const baseMonths = Math.floor(baseDays / 30);
+	const baseYears = Math.floor(baseDays / 365);
+
+	let milliseconds: number = baseMilliseconds;
+	let seconds = baseSeconds;
+	let minutes = baseMinutes;
+	let hours = baseHours;
+	let days = baseDays;
+	let months = baseMonths;
+	const years = baseYears;
+
+	if(format.includes("\\s")) milliseconds = baseMilliseconds % 1000; // Seconds
+	if(format.includes("\\m")) seconds = baseSeconds % 60; // Minutes
+	if(format.includes("\\h") || format.includes("\\H")) minutes = baseMinutes % 60;	// Hours
+	if(format.includes("\\d") || format.includes("\\D")) hours = baseHours % 24; // Days
+	if(format.includes("\\M")) days = baseDays % 30; // Months
+	if(format.includes("\\y") || format.includes("\\Y")) months = months % 12; // Years
+
+	const duration = format.replace(/\\S/, milliseconds.toString())
+		.replace(/\\s/g, seconds.toString())
+		.replace(/\\m/g, minutes.toString())
+		.replace(/\\h/g, hours.toString())
+		.replace(/\\H/g, hours.toString())
+		.replace(/\\d/g, days.toString())
+		.replace(/\\D/g, days.toString())
+		.replace(/\\M/g, months.toString())
+		.replace(/\\y/g, years.toString())
+		.replace(/\\Y/g, years.toString());
+
+	return duration;
+}
+
+function updateCountdown(){
+	try {
+		let human = startTime - Date.now();
+		if(human < 0) human = 0;
+		const time = humanTime(human, "\\d days \\h hours \\m minutes \\s seconds");
+
+		countdown.innerText = time;
+	} catch (e){
+		return e;
+	}
+}
+
+
+
 function getCookie(cookieName: string){
 	const name = `${cookieName}=`;
 	const decodedCookie = decodeURIComponent(document.cookie);
@@ -106,8 +179,7 @@ async function init(){
 	}
 
 	await connectToWebsocket();
-
-
+	setInterval(updateCountdown, 1000);
 }
 
 
